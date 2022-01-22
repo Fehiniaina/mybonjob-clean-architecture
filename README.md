d7a8835d028cd2a15d852ec8536c82f5f950216f
Fehiniaina <fehiniaina@hightao-mg.com>
2022-01-22 18:35:15 +0300

    Config docker

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # MyBonJob - Clean Architecture
+```
+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
+https://levelup.gitconnected.com/fix-password-authentication-github-3395e579ce74
+```
 
 ## clone le projet
 Faite un cd dans votre répertoire www/, puis git clone le projet
 ```bash
 $ git clone git@gitlab.com:bocasay/symfony/onerba.git
+```
+
+
+## Build du docker-compose
+Pour build et up le service
+```bash
+# mettez-vous dans le dossier racine du projet
+$ ./docker/php/docker-compose-1.23 -f docker-compose.yml -f local.yml up --build -d
 ```
--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -6,29 +6,14 @@ if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm "$@"
 fi
 
-if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
-    composer dump-autoload --optimize --no-dev --classmap-authoritative
-    composer install --prefer-dist --no-progress --no-suggest --no-interaction --optimize-autoloader
-    bin/console assets:install public
+#if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
+    #composer dump-autoload --optimize --no-dev --classmap-authoritative
+    #composer install --prefer-dist --no-progress --no-suggest --no-interaction --optimize-autoloader
 
     #drop all schema && migrate
-    bin/console doctrine:schema:drop --force --full-database
-    bin/console doctrine:migrations:migrate
-    bin/console doctrine:fixtures:load
+    #bin/console doctrine:schema:drop --force --full-database
+    #bin/console doctrine:migrations:migrate
+    #bin/console doctrine:fixtures:load
+#fi
 
-    #generer toutes les templates dust
-    bin/console generate:dustTemplates
-    bin/console dust:generate:templates
-
-    bin/console fos:js-routing:dump --format=json --target=public/js/fos_js_routes.json
-    yarn install
-    yarn encore dev
-    #yarn encore production
-
-    bin/phpunit
-
-	# Permissions hack because setfacl does not work on Mac and Windows
-	chown -R www-data var
-fi
-
-exec docker-php-entrypoint "$@"
+exec docker-entrypoint "$@"
